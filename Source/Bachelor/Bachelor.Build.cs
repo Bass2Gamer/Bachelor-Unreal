7e64178076f70ffdaef3174ec4be13c6146337b7
MelihM67 <mandaci-melih@hotmail.com>
2025-06-12 15:29:09 +0200

    aktueller stand save

--- Source/Bachelor/Bachelor.Build.cs
+++ Source/Bachelor/Bachelor.Build.cs
@@ -8,7 +8,18 @@ public class Bachelor : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{ 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore",
+			"EnhancedInput",
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks",
+			"AIModule",
+        });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
---
+++ Source/Bachelor/Public/GAS/PlayerAttributeSet.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "PlayerAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+
+/**
+ * 
+ */
+UCLASS()
+class BACHELOR_API UPlayerAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
